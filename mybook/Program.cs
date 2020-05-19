965e7b6c3d1f19c7b49803386d09e31c9ca69b5d
JINNNNS <baekhuen@naver.com>
2020-05-19 23:08:29 +0900

    초기 커밋.

---
+++ mybook/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mybook
+{
+    class MyBook
+    {
+        public string name;
+        public string date;
+        public string name2;
+        public string name3;
+        public string place;
+        public string editer;
+        public string idea;
+        public string edit;
+        public string design;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBook mybook = new MyBook
+            {
+                name = "PHP 프로그래밍 입문",
+                date = "2018년 5월 20일",
+                name2 = "황재호",
+                name3 = "김태헌",
+                place = "한빛아카데미(주)",
+                editer = "김현용",
+                idea = "김이화",
+                edit = "김이화",
+                design = "여동일"
+            };
+
+        }
+    }
+}
